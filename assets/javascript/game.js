9545c42f9f064f7f3c7a1a964a37a69139ed5a3b
Scott Foster <thescottfoster@gmail.com>
2017-09-24 11:43:04 -0500

    completed basic structure, logic; debugging and polish needed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,53 +2,173 @@
 
 var gameMode = "playerSelect";
 var knights = [];
-var playerKnight = null;
-var opponentKnights = [];
-
+var player = null;
+var defenderIndex = null;
+var knightsDefeated = 0;
 
 //--- --- Global Function / Object Definitions --- ---
 
 //constructor array for knights
 function Knight(name, hp, attack, counterattack, isPlayer, isDefender) {
   this.name = name;
-  this.hp = attack;
+  this.hp = hp;
+  this.attack = attack;
   this.counterattack = counterattack;
   this.isPlayer = isPlayer;
   this.isDefender = isDefender;
 }
 
-//set knights back to initial state, restart game mode
+//set knights back to initial state, restart game mode, hides win/loss panels
 function initialize(){
+	knights = [];
 	knights[0] = new Knight('Lancelot', 100, 10, 10, false, false);
-	knights[1] = new Knight('Galahad', 100, 10, 10, false, false);
-	knights[2] = new Knight('Kay', 100, 10, 10, false, false);
+	knights[1] = new Knight('Galahad', 30, 10, 10, false, false);
+	knights[2] = new Knight('Kay', 110, 10, 10, false, false);
 	knights[3] = new Knight('Gawain', 100, 10, 10, false, false);
 
+	var knightsDefeated = 0;
+
 	gameMode = "playerSelect";
+
+	$('#victory-panel').hide();
+	$('#defeat-panel').hide();
 
 	drawScreen();
 }
 
+//performs attack and couterattack calculations, checks win/lose conditions
+function attackRound(){
+
+	knights[defenderIndex].hp -= player.attack;
+	$('#battle-output').text('You attack '+knights[defenderIndex].name +' for ' +player.attack +' damage!');
+	player.attack += player.attack;
+
+	if(knights[defenderIndex].hp < 1){
+			knights[defenderIndex].hp = 0;
+			$('#battle-output').append('<h3>' + knights[defenderIndex].name + ' is defeated!</h3>');
+			knightsDefeated++;
+	}
+
+	else{
+		player.hp -= knights[defenderIndex].counterattack;
+		$('#battle-output').append('<h3>' + knights[defenderIndex].name + ' hits back for ' + knights[defenderIndex].counterattack + ' damage!</h3>');
+	}
+
+	if(player.hp < 1){
+		gameMode = 'defeat';
+	}
+
+	else if(knightsDefeated === knights.length){
+		gameMode = 'victory';
+	}
+
+//INSERT WINGAME / LOSEGAME CHECK
+
+	// if(player.hp < 1){
+	// 	gameMode = 'defeat';
+	// }
+
+	// if(knightsDefeated === knights.length){
+	// 	gameMode = 'victory';
+	// }
+
+	drawScreen();
+
+}
+
+
+
+//evaluates game mode and displays appropriate elements
 function drawScreen(){
 	switch(gameMode){
 		case "playerSelect": 
-			$('#player-select-panel').removeClass('hidden');
-			$('#opponent-select-panel').addClass('hidden');
-			$('#battle-panel').addClass('hidden');
+			//Shows only player select panel
+			$('#player-select-panel').show();
+			$('#opponent-select-panel').hide();
+			$('#battle-panel').hide();
+
+			
+			//Fills each column with HTML snippet for each knight
+			for(i = 0; i < knights.length; i++){
+				$('#player-select-' +i).html(
+					'<img src="assets/images/'+knights[i].name+'.png"><h2>'+knights[i].name+'</h2><h3>'+knights[i].hp+'</h3>'
+				);
+			}
+
 			break;		
 
 		case "opponentSelect":
-			$('#player-select-panel').addClass('hidden');
-			$('#opponent-select-panel').removeClass('hidden');
-			$('#battle-panel').addClass('hidden');
+			// Shows only opponent select panel
+			$('#player-select-panel').hide();
+			$('#opponent-select-panel').show();
+			$('#battle-panel').hide();
+
+			//Fills each column with HTML snippet for each knight
+			for(i = 0; i < knights.length; i++){
+				//If opponent already defeated, adds defeated class
+				if(knights[i].hp < 1){
+					$('#opponent-select-' +i).addClass('defeated');
+					// $('#opponent-select-' +i).removeClass('opponent-select-box');
+				}
+				else{
+					$('#opponent-select-' +i).removeClass('defeated');
+				}
+
+				$('#opponent-select-' +i).html(
+					'<img src="assets/images/'+knights[i].name+'.png"><h2>'+knights[i].name+'</h2><h3>'+knights[i].hp+'</h3>'
+				);
+			}
+
 			break;
 
 		case "battle":
-			$('#player-select-panel').addClass('hidden');
-			$('#opponent-select-panel').addClass('hidden');
-			$('#battle-panel').removeClass('hidden');
+			$('#player-select-panel').hide();
+			$('#opponent-select-panel').hide();
+			$('#battle-panel').show();
+
+			//Only show attack button until opponent defeated, then show next button
+			if( knights[defenderIndex].hp < 1 ){
+				$('#attack-button').hide();
+				$('#next-opponent-button').show();
+			}
+			else{
+				$('#attack-button').show();
+				$('#next-opponent-button').hide();
+			}
+
+			$('#player-image').html(
+					'<img src="assets/images/'+player.name+'.png">'
+				);
+			$('#player-name').text(player.name);
+			$('#player-hp').text(player.hp);
+
+
+			$('#defender-image').html(
+					'<img src="assets/images/'+knights[defenderIndex].name+'.png">'
+				);
+			$('#opponent-name').text(knights[defenderIndex].name);
+			$('#opponent-hp').text(knights[defenderIndex].hp);
+
+			//Win/Loss Check
+
+			
+
 			break;		
-				
+
+		case "victory":
+			$('#player-select-panel').hide();
+			$('#opponent-select-panel').hide();
+			$('#battle-panel').hide();
+			$('#victory-panel').show();
+			break;
+
+		case "defeat":
+			$('#player-select-panel').hide();
+			$('#opponent-select-panel').hide();
+			$('#battle-panel').hide();
+			$('#defeat-panel').show();
+			break;
+
 	}
 }
 
@@ -56,12 +176,54 @@ function drawScreen(){
 
 initialize();
 
+console.log(knights);
 
 
-for( i = 0; i < knights.length; i++){
-	console.log(knights[i]);
-}
+//When Player Select knight is clicked, selected knight object is assigned to Player and removed from Knights array
+$('.player-select-box').on("click", function(){
+	
+	player = (knights[this.id[14]]);
+	knights.splice([this.id[14]], 1);
 
+	console.log('Player: ' +player.name);
+
+	gameMode = "opponentSelect";
+	drawScreen();
+});
+
+$('.opponent-select-box').on("click", function(){
+	
+	//if a knight is already defeated, cannot be selected
+	if(knights[this.id[16]].hp < 1){
+		return;
+	}
+
+	defenderIndex = [this.id[16]];
+
+	gameMode = "battle";
+
+	console.log("Defender: " +knights[defenderIndex].name);
+	drawScreen();
+});
+
+$('#attack-button').on("click", function(){
+	
+	attackRound();
+
+});
+
+$('#next-opponent-button').on("click", function(){
+	
+	gameMode = 'opponentSelect';
+	drawScreen();
+
+});
+
+$('.restart-button').on("click", function(){
+
+	initialize();
+
+});
 
 
 
@@ -92,4 +254,14 @@ Drawscreen:
 Initialize:
 	Set counters to 0 except Wins / Losses
 
+
+	"Through your prowess at arms, you have defeated X Y and Z and recovered the Holy Grail for Camelot."
+
+
+NOTES TO SELF: Reset currently not fully restarting, some counters remain; player victorious after one battle following prior victory
+	CONSOLE LOG ALL VARIABLES AFTER EACH BATTLE
+
+
 */
+
+
